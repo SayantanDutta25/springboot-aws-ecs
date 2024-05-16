f6933e4e604fa845aaf8f57579fb9142faa8e474
SayantanDutta25 <85352496+SayantanDutta25@users.noreply.github.com>
2024-05-15 23:35:40 -0400

    adding @version

--- src/main/java/com/employee/management/model/Department.java
+++ src/main/java/com/employee/management/model/Department.java
@@ -1,61 +1,58 @@
-package com.employee.management.model;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
 @Entity
-@Table(name="department")
+@Table(name = "department")
 public class Department {
 
-	@Id
-	@Column(name="department_id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int department_ID;
-	private String short_Name;
-	private String department_Name;
-	
-	public Department() {
-	
-	}
-	
-	public Department(int departmentID){
-		super();
-		this.department_ID = departmentID;
-	}
-	
-	public Department(int departmentID, String short_Name, String department_Name) {
-		super();
-		this.department_ID = departmentID;
-		this.short_Name = short_Name;
-		this.department_Name = department_Name;
-	}
+    @Id
+    @Column(name = "department_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int department_ID;
+    
+    @Column(name = "short_name")
+    private String short_Name;
+    
+    @Column(name = "department_name")
+    private String department_Name;
+    
+    @Version
+    private int version;
 
-	public int getDepartment_ID() {
-		return department_ID;
-	}
+    public Department() {
+    }
 
-	public void setDepartment_ID(int department_ID) {
-		this.department_ID = department_ID;
-	}
+    public Department(int departmentID) {
+        super();
+        this.department_ID = departmentID;
+    }
 
-	public String getShort_Name() {
-		return short_Name;
-	}
+    public Department(int departmentID, String short_Name, String department_Name) {
+        super();
+        this.department_ID = departmentID;
+        this.short_Name = short_Name;
+        this.department_Name = department_Name;
+    }
 
-	public void setShort_Name(String short_Name) {
-		this.short_Name = short_Name;
-	}
+    public int getDepartment_ID() {
+        return department_ID;
+    }
 
-	public String getDepartment_Name() {
-		return department_Name;
-	}
+    public void setDepartment_ID(int department_ID) {
+        this.department_ID = department_ID;
+    }
 
-	public void setDepartment_Name(String department_Name) {
-		this.department_Name = department_Name;
-	}
+    public String getShort_Name() {
+        return short_Name;
+    }
+
+    public void setShort_Name(String short_Name) {
+        this.short_Name = short_Name;
+    }
+
+    public String getDepartment_Name() {
+        return department_Name;
+    }
+
+    public void setDepartment_Name(String department_Name) {
+        this.department_Name = department_Name;
+    }
 
 }
