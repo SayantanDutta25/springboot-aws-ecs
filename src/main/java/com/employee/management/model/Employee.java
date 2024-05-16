07bbff1de9a586d4f72f164022434e89eaef1d93
SayantanDutta25 <85352496+SayantanDutta25@users.noreply.github.com>
2024-05-15 23:54:52 -0400

    Revert "adding @version"
    This reverts commit f6933e4e604fa845aaf8f57579fb9142faa8e474.

--- src/main/java/com/employee/management/model/Employee.java
+++ src/main/java/com/employee/management/model/Employee.java
@@ -1,90 +1,95 @@
 package com.employee.management.model;
 
 import org.antlr.v4.runtime.misc.NotNull;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "employee")
+@Table (name = "employee")
 public class Employee {
 
-    @Id
-    @Column(name = "employee_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int employeeID;
+	@Id
+	@Column(name="employee_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int employeeID;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name="department_id")
+	private Department department;
+	
+	@ManyToOne
+	@JoinColumn(name="salary_id")
+	private Salary salary;
+	
+	public Employee(){
+		
+	}
+	
+	public Employee(String firstName, String lastName, Department department) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.department = department;
+	}
+	
+	public int getEmployeeID() {
+		return employeeID;
+	}
 
-    @Column(name = "first_name")
-    private String firstName;
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
 
-    @Column(name = "last_name")
-    private String lastName;
+	public String getFirstName() {
+		return firstName;
+	}
 
-    @NotNull
-    @ManyToOne
-    @JoinColumn(name = "department_id")
-    private Department department;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    @ManyToOne
-    @JoinColumn(name = "salary_id")
-    private Salary salary;
+	public String getLastName() {
+		return lastName;
+	}
 
-    @Version
-    private int version;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public Employee() {
+	@Override
+	public String toString(){
+		return String.format("Employee [employeeID = %d, firstName = %s, lastName = %s, department_ID= %d", employeeID, firstName, lastName, department.getDepartment_ID());
+	}
 
-    }
+	public Department getDepartment() {
+		return department;
+	}
 
-    public Employee(String firstName, String lastName, Department department) {
-        super();
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.department = department;
-    }
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	public Salary getSalary() {
+		return salary;
+	}
 
-    public int getEmployeeID() {
-        return employeeID;
-    }
+	public void setSalary(Salary salary) {
+		this.salary = salary;
+	}
 
-    public void setEmployeeID(int employeeID) {
-        this.employeeID = employeeID;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Employee [employeeID = %d, firstName = %s, lastName = %s, department_ID= %d", employeeID,
-                firstName, lastName, department.getDepartment_ID());
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
-    public Salary getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Salary salary) {
-        this.salary = salary;
-    }
-
+	
 }
--- src/main/java/com/employee/management/model/Salary.java
+++ src/main/java/com/employee/management/model/Salary.java
@@ -6,78 +6,74 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Version;
 
 @Entity
-@Table(name = "salary")
+@Table(name="salary")
 public class Salary {
 
-    @Id
-    @Column(name = "salary_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int salary_ID;
-
-    @Column(name = "`range`") // Enclose 'range' in backticks to avoid conflict with reserved keyword
+	@Id
+	@Column(name="salary_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int salary_ID;
+	
+	@Column(name = "`range`") // Enclose 'range' in backticks to avoid conflict with reserved keyword
     private String range;
-
-    private double amount;
-
+	
+	private double amount;
+	
     @Column(name = "increased_salary")
     private double increasedSalary;
+	
+	public Salary() {
+	
+	}
+	
+	public Salary(int salaryID){
+		super();
+		this.salary_ID = salaryID;
+	}
+	
+	public Salary(int salaryID, String range, int amount, int increasedSal) {
+		super();
+		this.salary_ID = salaryID;
+		this.range = range;
+		this.amount = amount;
+		this.increasedSalary = increasedSal;
+	}
 
-    @Version
-    private int version;
+	public int getSalary_ID() {
+		return salary_ID;
+	}
 
-    public Salary() {
+	public void setSalary_ID(int salary_ID) {
+		this.salary_ID = salary_ID;
+	}
 
-    }
+	public String getRange() {
+		return range;
+	}
 
-    public Salary(int salaryID) {
-        super();
-        this.salary_ID = salaryID;
-    }
+	public void setRange(String range) {
+		this.range = range;
+	}
 
-    public Salary(int salaryID, String range, int amount, int increasedSal) {
-        super();
-        this.salary_ID = salaryID;
-        this.range = range;
-        this.amount = amount;
-        this.increasedSalary = increasedSal;
-    }
+	public double getAmount() {
+		return amount;
+	}
 
-    public int getSalary_ID() {
-        return salary_ID;
-    }
-
-    public void setSalary_ID(int salary_ID) {
-        this.salary_ID = salary_ID;
-    }
-
-    public String getRange() {
-        return range;
-    }
-
-    public void setRange(String range) {
-        this.range = range;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public double getIncreasedSalary() {
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	public double getIncreasedSalary() {
         return increasedSalary;
     }
-
-    public void setIncreasedSalary(double increasedSalary) {
+	
+	public void setIncreasedSalary(double increasedSalary) {
         this.increasedSalary = increasedSalary;
     }
-
-    public static class IncrementRequest {
+	
+	public static class IncrementRequest {
         private double percentage;
 
         public double getPercentage() {
@@ -88,4 +84,5 @@ public class Salary {
             this.percentage = percentage;
         }
     }
+
 }
